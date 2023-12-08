129568be7af5ad39a2a0d83bc396bcb448b4616d
Reef Salter <reefsalter@gmail.com>
2023-12-08 12:27:50 +0000

    finished day 1

--- 2023/Day 1/aoc1.py
+++ 2023/Day 1/aoc1.py
@@ -2,8 +2,9 @@ total = 0
 with open('/workspaces/AoC/2023/Day 1/aoc1.txt') as f:
    for line in f:
     n =''.join(i for i in line if i.isdigit())
-    first_digit = int(n[0])
-    last_digit = int(n[-1])
+    first_digit = str(n[0])
+    last_digit = str(n[-1])
     sum = first_digit + last_digit
+    sum = int(sum)
     total = total + sum
 print(total)
